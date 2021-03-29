01b79d30016fcaae84dba4c2b51dcf841da6fb7d
mamontovdn <mamontovdn@yandex.ru>
2021-03-29 12:47:24 +0300

    init2

--- src/Schema/Schema.jsx
+++ src/Schema/Schema.jsx
@@ -1,8 +1,8 @@
 import {Image} from "react-bootstrap";
-
+import img from "./mnemo.png"
 export const Schema = () => {
     return (
-            <Image src={'mnemo.png'} style={{height:"550px", width:"100%"}} fluid/>
+            <Image src={img} style={{height:"550px", width:"100%"}} fluid/>
     )
 }
 
